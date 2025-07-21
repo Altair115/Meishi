8775afe0f5323f0ca189cbe95f39bbe52d110721
Altair115 <kevin.celinski@outlook.com>
2025-07-21 15:57:16 +0200

    :added: Basic router and homepage to test no fancy layouts yet

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+﻿import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+// Import more views here to add them
+
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView,
+    },
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(), // Hash mode for GitHub Pages compatibility
+    routes,
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to Meishi!</h1>
+    <p>This is your homepage. Edit it as you like!</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
