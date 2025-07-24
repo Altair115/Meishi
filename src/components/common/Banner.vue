1576d65deb9c0edeea878062086524592bb444ec
Altair115 <kevin.celinski@outlook.com>
2025-07-24 12:18:39 +0200

    :sparkles: Added basic Herobanner (resuable)

---
+++ src/components/common/Banner.vue
@@ -0,0 +1,26 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="hero-banner">
+    <slot>
+      <!-- Default content if no slot is provided -->
+      <h1>Welcome!</h1>
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+.hero-banner {
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to bottom right, #80d0c7, #0093e9);
+  color: #fff;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 ﻿<script setup lang="ts">
-
+import Banner from "@/components/common/Banner.vue";
 </script>
 
 <template>
+  <Banner>
+    <h1>K.E. Celinski</h1>
+    <p>Software engineer, Programmer, Game designer</p>
+  </Banner>
+
   <div>
     <h1>Welcome to Meishi!</h1>
     <p>This is your homepage. Edit it as you like!</p>
